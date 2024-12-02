505decf69d3b66359957616d7bb234951d174cd7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-02 00:27:01 +0000

    Day 1 Complete

--- AOC/Day1/Day1.cs
+++ AOC/Day1/Day1.cs
@@ -4,6 +4,24 @@ public class Day1
 {
     public int GetTotalDistance(string input)
     {
+        var (list1, list2) = GetLists(input);
+        
+        return list1
+            .Zip(list2, GetDifference)
+            .Sum();
+    }
+    
+    public int GetSimilaryScore(string input)
+    {
+        var (list1, list2) = GetLists(input);
+
+        return list1
+            .Select(x => x * list2.Count(y => y == x))
+            .Sum();
+    }
+
+    private (IEnumerable<int> list1, IEnumerable<int> list2) GetLists(string input)
+    {
         var lists = input.Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
             .Select(x => x.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
             .Select(x => (int.Parse(x[0]), int.Parse(x[1])))
@@ -11,10 +29,8 @@ public class Day1
 
         var list1 = lists.Select(x => x.Item1).Order();
         var list2 = lists.Select(x => x.Item2).Order();
-        
-        return list1
-            .Zip(list2, GetDifference)
-            .Sum();
+
+        return (list1, list2);
     }
 
     private int GetDifference(int x, int y)
---
+++ AOC/Test.Utilities/TestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Test.Utilities;
+
+public abstract class TestBase
+{
+    protected static string Input { get; private set; } = null!;
+    
+    [Before(Class)]
+    public static async Task Setup()
+    {
+        Input = await File.ReadAllTextAsync("input.txt");
+    }
+}
